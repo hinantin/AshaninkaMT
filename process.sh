0b15c19bcff61f7dd46d05772554e80136d2652d
rcastromamani <richard.castro.mamani@gmail.com>
2017-08-10 09:31:57 -0500

    Adding more vocab.

--- process.sh
+++ process.sh
@@ -2,19 +2,16 @@
 
 export FREELINGSHARE=/usr/local/share/freeling
 export LD_LIBRARY_PATH=/usr/local/lib
-SPANISH=/usr/local/share/freeling/config
+CONFIG=/usr/local/share/freeling/config
 
 # sh process.sh myinput.txt myoutput.txt
 
 INPUT=$1
 OUTPUT=$2
 
-gedit $INPUT 
+/usr/local/bin/analyzer -f $CONFIG/en.cfg --outlv parsed --output conll <$INPUT >$OUTPUT
 
-/usr/local/bin/analyzer -f $SPANISH/es.cfg --phon <$INPUT >$OUTPUT
-
-gedit $OUTPUT 
+cat $OUTPUT 
 
 
 #/usr/local/bin/analyzer --server -p 9696 -f /usr/local/share/freeling/config/es.cfg --outlv dep --output conll
-
