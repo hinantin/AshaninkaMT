4b3a9ce26df96a50d0cc6f04a6ca02b668651092
root <richard.castro.mamani@gmail.com>
2020-05-23 20:52:21 +0000

    Adding changes.

--- translate.sh
+++ translate.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
+FREELINGINSTALLPATH=/home/hinantin/FreeLing_install
+MATXININSTALLPATH=/home/hinantin/squoia/MT_systems/matxin-lex
+TEMPPATH=/tmp
+export FREELINGSHARE=$FREELINGINSTALLPATH/share/freeling
+export LD_LIBRARY_PATH=$FREELINGINSTALLPATH/lib
+CONFIG=$FREELINGINSTALLPATH/share/freeling/config
 
-export FREELINGSHARE=/usr/local/share/freeling
-export LD_LIBRARY_PATH=/usr/local/lib
-CONFIG=/usr/local/share/freeling/config
-
+# DEFAULT CONFIGURATION 
+#export FREELINGSHARE=/usr/local/share/freeling
+#export LD_LIBRARY_PATH=/usr/local/lib
+#CONFIG=/usr/local/share/freeling/config
 
 # sh process.sh myinput.txt myoutput.txt
 
-INPUT=myinput.txt
-OUTPUT=myoutput.txt
+INPUT=$TEMPPATH/myinput.txt
+OUTPUT=$TEMPPATH/myoutput.txt
 
 rm -f $INPUT $OUTPUT 
 
 printf "$1" > $INPUT
 
-/usr/local/bin/analyzer_client localhost:50005 <$INPUT >$OUTPUT
+$FREELINGINSTALLPATH/bin/analyzer_client localhost:50005 <$INPUT >$OUTPUT
 
 #/usr/local/bin/analyzer_client
 
 # cat $OUTPUT
 
-perl conll2xml.pm $OUTPUT | /home/richard/Documents/squoia/MT_systems/bin/squoia_xfer_lex en-cni.bin
+perl conll2xml.pm $OUTPUT | $MATXININSTALLPATH/squoia_xfer_lex en-cni.bin
 
-
-
-#/usr/local/bin/analyzer --server -p 9696 -f /usr/local/share/freeling/config/es.cfg --outlv dep --output conll
